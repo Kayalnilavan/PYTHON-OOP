cc1a0e285e75dd0c8fcedec273c5d89cee86fa98
Kayalnilavan <kayalnilavan110@gmail.com>
2026-03-12 23:54:23 +0530

    Add Initial file
    This Python code demonstrates the creation and use of a simple class and object. First, a class named `Student` is defined using the `class` keyword. The `pass` statement is used as a placeholder to indicate that the class currently has no attributes or methods. An object named `Stu1` is then created from the `Student` class. The `print(Stu1)` statement displays the memory reference of the object, while `print(type(Stu1))` shows that the object belongs to the `Student` class. After creating the object, attributes are dynamically assigned to it: `name` is set to `"Kayal"` and `id` is set to `1000`. Finally, the program prints these attributes using `print(Stu1.name)` and `print(Stu1.id)`. This example illustrates the basic concept of object-oriented programming in Python, including class definition, object creation, and dynamic attribute assignment.

---
+++ 2026 MAR 11/Student1.py
@@ -0,0 +1,11 @@
+class Student:
+    pass                #Act as placeholder for an empty class
+
+Stu1 = Student()        #Creating an object
+print(Stu1)
+print(type(Stu1))
+
+Stu1.name="Kayal"
+Stu1.id=1000
+print(Stu1.name)
+print(Stu1.id)
